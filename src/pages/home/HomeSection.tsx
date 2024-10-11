62e0aef7ac7a08c97596028402f0c6846b4bdbc2
Naveedahmedtech <technaveedahmed@gmail.com>
2024-10-11 08:28:43 +0500

    home section

--- src/pages/home/HomeSection.tsx
+++ src/pages/home/HomeSection.tsx
@@ -30,7 +30,7 @@ function HomeSection() {
         >
             <div className="absolute inset-0 bg-gradient-to-br from-gray-900 to-blue-900 opacity-50 blur-2xl"></div>
             <motion.div
-                className="relative w-full max-w-5xl text-textPrimary p-4 md:p-12 text-center bg-gradient-to-br from-gray-900 bg-opacity-80 rounded-3xl shadow-2xl"
+                className="relative w-full max-w-5xl text-textPrimary p-4 py-12 md:p-12 text-center bg-gradient-to-br from-gray-900 bg-opacity-80 rounded-3xl shadow-2xl"
                 variants={childVariants}
             >
                 <motion.h2
@@ -53,16 +53,18 @@ function HomeSection() {
                     className="flex flex-col md:flex-row justify-center space-y-4 md:space-y-0 md:space-x-6 mb-6 sm:mb-8"
                     variants={childVariants}
                 >
-                    <motion.button
+                    <motion.a
+                        href="#projects"
                         className="bg-blue-600 hover:bg-blue-800 px-4 sm:px-6 md:px-8 py-2 sm:py-3 md:py-4 rounded-full font-semibold shadow-lg hover:scale-110 transform transition-all duration-100 ease-in-out focus:outline-none ring-4 ring-offset-2 ring-blue-800"
                     >
-                        <a href="#projects">Explore My Work</a>
-                    </motion.button>
-                    <motion.button
+                        Explore My Work
+                    </motion.a>
+                    <motion.a
+                        href="#contact"
                         className="bg-green-600 hover:bg-green-800 px-4 sm:px-6 md:px-8 py-2 sm:py-3 md:py-4 rounded-full font-semibold shadow-lg transform transition-all duration-100 ease-in-out focus:outline-none focus:ring-4 focus:ring-offset-2 focus:ring-green-800 hover:scale-110 hover:ring-4 hover:ring-offset-2 hover:ring-green-800"
                     >
-                        <a href="#contact">Let’s Collaborate</a>
-                    </motion.button>
+                        Let’s Collaborate
+                    </motion.a>
                 </motion.div>
                 <motion.div className="flex justify-center space-x-6 md:space-x-8 text-2xl md:text-3xl text-accent" variants={childVariants}>
                     <motion.a href="https://github.com/Naveedahmedtech" target="_blank" whileHover={{ scale: 1.2 }}>
