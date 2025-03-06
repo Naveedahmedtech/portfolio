70cd2b3e69b00aa8a33406e22dfb41d81842e9d1
daniayltariq <daniyaldottariq@gmail.com>
2025-03-06 06:45:12 +0500

    testing github workflows

--- src/layout/header/components/Logo.tsx
+++ src/layout/header/components/Logo.tsx
@@ -1,7 +1,7 @@
 export default function Logo() {
     return (
         <h1 className="text-textPrimary dark:text-textPrimary text-4xl font-bold tracking-tight hover:opacity-80 transition-opacity duration-300">
-            <span className="glow-effect">Naveed Ahmed</span>
+            <span className="glow-effect">Naveed Ahmed Tech</span>
         </h1>
     );
 }
